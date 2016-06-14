025567e45d91595745e57e57bbfab575919c9f9c
dake <chen423@yeah.net>
2016-06-14 14:23:00 +0800

    code refactor

--- BaseUtlity/Extension/NSObject+TCUtilities.h
+++ BaseUtlity/Extension/NSObject+TCUtilities.h
@@ -6,6 +6,8 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 typedef struct _TCSwizzleInput {
     __unsafe_unretained Class klass;
     SEL srcSel;
@@ -14,7 +16,8 @@ typedef struct _TCSwizzleInput {
     
 } TCSwizzleInput;
 
-extern BOOL tcSwizzleMethod(TCSwizzleInput input, id block, IMP *origIMP, NSError **err);
+extern BOOL tcSwizzleMethod(TCSwizzleInput input, _Nullable id block, _Nullable IMP *_Nullable origIMP, NSError **err);
+
 
 
 @interface NSObject (TCUtilities)
@@ -31,8 +34,8 @@ extern BOOL tcSwizzleMethod(TCSwizzleInput input, id block, IMP *origIMP, NSErro
 
 // Request return value from performing selector
 - (id)objectByPerformingSelectorWithArguments:(SEL)selector, ...;
-- (__autoreleasing id)objectByPerformingSelector:(SEL)selector withObject:(id)object1 andObject:(id)object2;
-- (id)objectByPerformingSelector:(SEL)selector withObject:(id)object1;
+- (__autoreleasing id)objectByPerformingSelector:(SEL)selector withObject:(_Nullable id)object1 andObject:(_Nullable id)object2;
+- (id)objectByPerformingSelector:(SEL)selector withObject:(_Nullable id)object1;
 - (id)objectByPerformingSelector:(SEL)selector;
 
 // Delay Utilities
@@ -79,3 +82,144 @@ extern BOOL tcSwizzleMethod(TCSwizzleInput input, id block, IMP *origIMP, NSErro
 #endif
 
 @end
+
+
+#pragma mark - BKAssociatedObjects
+
+@interface NSObject (BKAssociatedObjects)
+
+/** Strongly associates an object with the reciever.
+ 
+ The associated value is retained as if it were a property
+ synthesized with `nonatomic` and `retain`.
+ 
+ Using retained association is strongly recommended for most
+ Objective-C object derivative of NSObject, particularly
+ when it is subject to being externally released or is in an
+ `NSAutoreleasePool`.
+ 
+ @param value Any object.
+ @param key A unique key pointer.
+ */
+- (void)bk_associateValue:(nullable id)value withKey:(const void *)key;
+
+/** Strongly associates an object with the receiving class.
+ 
+ @see associateValue:withKey:
+ @param value Any object.
+ @param key A unique key pointer.
+ */
++ (void)bk_associateValue:(nullable id)value withKey:(const void *)key;
+
+/** Strongly, thread-safely associates an object with the reciever.
+ 
+ The associated value is retained as if it were a property
+ synthesized with `atomic` and `retain`.
+ 
+ Using retained association is strongly recommended for most
+ Objective-C object derivative of NSObject, particularly
+ when it is subject to being externally released or is in an
+ `NSAutoreleasePool`.
+ 
+ @see associateValue:withKey:
+ @param value Any object.
+ @param key A unique key pointer.
+ */
+- (void)bk_atomicallyAssociateValue:(nullable id)value withKey:(const void *)key;
+
+/** Strongly, thread-safely associates an object with the receiving class.
+ 
+ @see associateValue:withKey:
+ @param value Any object.
+ @param key A unique key pointer.
+ */
++ (void)bk_atomicallyAssociateValue:(nullable id)value withKey:(const void *)key;
+
+/** Associates a copy of an object with the reciever.
+ 
+ The associated value is copied as if it were a property
+ synthesized with `nonatomic` and `copy`.
+ 
+ Using copied association is recommended for a block or
+ otherwise `NSCopying`-compliant instances like NSString.
+ 
+ @param value Any object, pointer, or value.
+ @param key A unique key pointer.
+ */
+- (void)bk_associateCopyOfValue:(nullable id)value withKey:(const void *)key;
+
+/** Associates a copy of an object with the receiving class.
+ 
+ @see associateCopyOfValue:withKey:
+ @param value Any object, pointer, or value.
+ @param key A unique key pointer.
+ */
++ (void)bk_associateCopyOfValue:(nullable id)value withKey:(const void *)key;
+
+/** Thread-safely associates a copy of an object with the reciever.
+ 
+ The associated value is copied as if it were a property
+ synthesized with `atomic` and `copy`.
+ 
+ Using copied association is recommended for a block or
+ otherwise `NSCopying`-compliant instances like NSString.
+ 
+ @see associateCopyOfValue:withKey:
+ @param value Any object, pointer, or value.
+ @param key A unique key pointer.
+ */
+- (void)bk_atomicallyAssociateCopyOfValue:(nullable id)value withKey:(const void *)key;
+
+/** Thread-safely associates a copy of an object with the receiving class.
+ 
+ @see associateCopyOfValue:withKey:
+ @param value Any object, pointer, or value.
+ @param key A unique key pointer.
+ */
++ (void)bk_atomicallyAssociateCopyOfValue:(nullable id)value withKey:(const void *)key;
+
+/** Weakly associates an object with the reciever.
+ 
+ A weak association will cause the pointer to be set to zero
+ or nil upon the disappearance of what it references;
+ in other words, the associated object is not kept alive.
+ 
+ @param value Any object.
+ @param key A unique key pointer.
+ */
+- (void)bk_weaklyAssociateValue:(nullable __autoreleasing id)value withKey:(const void *)key;
+
+/** Weakly associates an object with the receiving class.
+ 
+ @see weaklyAssociateValue:withKey:
+ @param value Any object.
+ @param key A unique key pointer.
+ */
++ (void)bk_weaklyAssociateValue:(nullable __autoreleasing id)value withKey:(const void *)key;
+
+/** Returns the associated value for a key on the reciever.
+ 
+ @param key A unique key pointer.
+ @return The object associated with the key, or `nil` if not found.
+ */
+- (nullable id)bk_associatedValueForKey:(const void *)key;
+
+/** Returns the associated value for a key on the receiving class.
+ 
+ @see associatedValueForKey:
+ @param key A unique key pointer.
+ @return The object associated with the key, or `nil` if not found.
+ */
++ (nullable id)bk_associatedValueForKey:(const void *)key;
+
+/** Returns the reciever to a clean state by removing all
+ associated objects, releasing them if necessary. */
+- (void)bk_removeAllAssociatedObjects;
+
+/** Returns the recieving class to a clean state by removing
+ all associated objects, releasing them if necessary. */
++ (void)bk_removeAllAssociatedObjects;
+
+@end
+
+NS_ASSUME_NONNULL_END
